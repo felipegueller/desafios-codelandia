f3fd4cefe9ebf2f8a50e95753f1072e16f108669
felipegueller <felipe.gueller@gmail.com>
2022-06-28 12:05:12 -0300

    feat: adding the animation by ScrollReveal library

---
+++ desafio1/scripts/main.js
@@ -0,0 +1,17 @@
+const post = ScrollReveal({
+  origin: 'top',
+  distance: '100px',
+  duration: 1000
+})
+
+const content = ScrollReveal({
+  origin: 'left',
+  distance: '100px',
+  duration: 1000
+})
+
+ScrollReveal().reveal('.post', post)
+ScrollReveal().reveal('.date', { delay: 200, ...content })
+ScrollReveal().reveal('.icon', {delay: 200, ...content })
+ScrollReveal().reveal('.title', { delay: 800, ...content })
+ScrollReveal().reveal('.content', { delay: 1400, ...content })
